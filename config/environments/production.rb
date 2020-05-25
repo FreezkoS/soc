f2b7a345d97df3ea3532a348351a1ae1d7dbcd93
freezkos <egor.uxanov.97@mail.ru>
2020-05-25 14:05:14 +0500

    serv

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     config.action_mailer.default_url_options = { host: host }
     ActionMailer::Base.smtp_settings = {
       :address              => 'smtp.sendgrid.net',
-      :port                 => '587',
+      :port                 => '80',
       :authentication       => :plain,
       :user_name            => ENV['SENDGRID_USERNAME'],
       :password             => ENV['SENDGRID_PASSWORD'],
